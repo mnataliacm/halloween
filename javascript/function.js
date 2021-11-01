ea2ff86378bdea3ebc16b1f5ddad1fa2563b3edc
Natalia Castillo Muñoz <74043250+mnataliacm@users.noreply.github.com>
2021-11-01 21:48:54 +0100

    Add files via upload

---
+++ javascript/function.js
@@ -0,0 +1,12 @@
+function mostrar(identificador){
+    document.getElementById(identificador).style.visibility = "visible";
+}
+function ocultar(identificador){
+    document.getElementById(identificador).style.visibility = "hidden";
+}
+function sonar(elemento){
+    document.getElementById(elemento).play();
+}
+function parar(elemento){
+    document.getElementById(elemento).pause();
+}
